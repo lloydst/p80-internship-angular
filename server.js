56e058e88c62cf8a5b1a9e76de7baefe47f27868
Lloyd Stumpel <lloyd.stumpel@poort80.nl>
2018-04-11 10:48:28 +0200

    api documented

--- server.js
+++ server.js
@@ -14,13 +14,13 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 // swagger definition
 var swaggerDefinition = {
-  info: {
-    title: 'Node Swagger API',
-    version: '1.0.0',
-    description: 'Demonstrating how to describe a RESTful API with Swagger',
+  info: { // API informations (required)
+    title: 'P80 api documentation', // Title (required)
+    version: '1.0.0', // Version (required)
+    description: 'api documentation for lloydst internship assignment angular/front end documentation can be found at http://localhost:8080 after `npm run doc` has been run', // Description (optional)
   },
-  host: `localhost`,
-  basePath: '/',
+  host: 'localhost:3000/', // Host (optional)
+  basePath: 'api', // Base path (optional)
 };
 var swaggerOptions = {
   // import swaggerDefinitions
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,17 +3,63 @@ var router = express.Router();
 var Website = require('../models/Website')
 var Message = require('../models/Message')
 var status = require('http-status');
-/**
- *  @swagger
- *  /:
- *    get:
- *      summary: says hi to the people
- *      operationId: index
- *      responses:
- *        200:
- *          description: hello world
- */
+
+/** #defines parameters
+   * @swagger
+   * definitions:
+   *   website:
+   *     required:
+   *       - id
+   *     properties:
+   *       name:
+   *         type: string
+   *       url:
+   *         type: string
+   *       visable:
+   *         type: boolean
+   *   message:
+   *     properties:
+   *       message:
+   *         type: string
+   *       showFrom:
+   *         type: string
+   *       showTill:
+   *         type: string
+   */  
+
+  /**
+   * @swagger
+   * tags:
+   *    - name: Website
+   *      description: Websites
+   *    - name: Message
+   *      description: Messages
+   */
+
+  /**
+   * @swagger
+   * /websites/:id:
+   *   put:
+   *     description: updates a website
+   *     tags: [Website]
+   *     produces:
+   *       - application/json
+   *     parameters:
+   *       - $ref: '#/parameters/id'
+   *       - name: id
+   *         description: the documents id to update
+   *         in: formData
+   *         required: true
+   *         type: string
+   *     responses:
+   *       200:
+   *         description: update
+   *         schema:
+   *           type: object
+   *           $ref: '#/definitions/website'
+   */
 router.put('/websites/:id', function(req,res) {
+    
     Website.findOneAndUpdate({name: req.params.id}, req.body,function(err, doc) {
         if(err) {
             res.send(err)
@@ -21,6 +67,21 @@ router.put('/websites/:id', function(req,res) {
         res.json({ message: 'Successfully updated' })
     })
 })
+/**
+   * @swagger
+   * /websites:
+   *   get:
+   *     description: gets all websites
+   *     tags: [Website]
+   *     produces:
+   *       - application/json
+   *     responses:
+   *       200:
+   *         description: recieved array of websites
+   *         schema:
+   *           type: object
+   *           $ref: '#/definitions/website'
+   */
 router.get('/websites',function(req, res) {
   Website.find(function(err, websites) {
       if (err)
@@ -30,6 +91,29 @@ router.get('/websites',function(req, res) {
       // res.status(200).json({message: 'SUCCESS', websites});
   });
 });
+
+/**
+   * @swagger
+   * /websites/:id:
+   *   get:
+   *     description: gets a specific website
+   *     tags: [Website]
+   *     produces:
+   *       - application/json
+   *     parameters:
+   *       - $ref: '#/parameters/id'
+   *       - name: id
+   *         description: the documents id to GET
+   *         in: formData
+   *         required: true
+   *         type: string
+   *     responses:
+   *       200:
+   *         description: GET
+   *         schema:
+   *           type: object
+   *           $ref: '#/definitions/website'
+   */
 router.get('/websites/:id', function(req,res) {
         Website.find({name: req.params.id}, function(err, website) {
             if (err)
@@ -37,6 +121,27 @@ router.get('/websites/:id', function(req,res) {
             res.json(website);
         });
     });
+    /**
+   * @swagger
+   * /websites/:id:
+   *   delete:
+   *     description: deletes a website
+   *     tags: [Website]
+   *     produces:
+   *       - application/json
+   *     parameters:
+   *       - $ref: '#/parameters/id'
+   *       - name: id
+   *         description: the documents id to delete
+   *         in: formData
+   *         required: true
+   *         type: string
+   *     responses:
+   *       200:
+   *         description: deleted
+   *         schema:
+   *           type: object
+   */
     router.delete('/websites/:id', function(req, res) {
         Website.remove({
             _id: req.params.id
@@ -47,6 +152,21 @@ router.get('/websites/:id', function(req,res) {
             res.json({ message: 'Successfully deleted' });
         })
     })
+    /**
+   * @swagger
+   * /websites:
+   *   post:
+   *     description: creates a website
+   *     tags: [Website]
+   *     produces:
+   *       - application/json
+   *     
+   *     responses:
+   *       200:
+   *         description: created
+   *         schema:
+   *           type: object
+   */
     router.post('/websites',function(req, res) {
         var site = new Website();      
         site.name = req.body.name;  
@@ -64,7 +184,28 @@ router.get('/websites/:id', function(req,res) {
 //ALL
 
 
-//SINGLE
+/**
+   * @swagger
+   * /messages/:id:
+   *   get:
+   *     description: gets a specific message
+   *     tags: [Message]
+   *     produces:
+   *       - application/json
+   *     parameters:
+   *       - $ref: '#/parameters/id'
+   *       - name: id
+   *         description: the documents id to Get
+   *         in: formData
+   *         required: true
+   *         type: string
+   *     responses:
+   *       200:
+   *         description: Get
+   *         schema:
+   *           type: object
+   *           $ref: '#/definitions/message'
+   */
 router.get('/messages/:id',function(req,res) {
     Message.findById(req.params.id, function(err, message) {
         if (err) {
@@ -74,7 +215,22 @@ router.get('/messages/:id',function(req,res) {
     })
 })
 
-//All
+/**
+   * @swagger
+   * /messages:
+   *   get:
+   *     description: gets all messages
+   *     tags: [Message]
+   *     produces:
+   *       - application/json
+
+   *     responses:
+   *       200:
+   *         description: Get
+   *         schema:
+   *           type: array
+   *           $ref: '#/definitions/message'
+   */
 router.get('/messages',function(req, res) {
     Message.find(function(err, messages) {
         if (err){
@@ -83,6 +239,21 @@ router.get('/messages',function(req, res) {
         res.json(messages);
     });
   });
+  /**
+   * @swagger
+   * /messages:
+   *   post:
+   *     description: creates a message
+   *     tags: [Message]
+   *     produces:
+   *       - application/json
+   *     responses:
+   *       200:
+   *         description: Post
+   *         schema:
+   *           type: object
+   *           $ref: '#/definitions/message'
+   */
   router.post('/messages',function(req, res) {
     var mssg = new Message();      // create a new instance of the Bear model
     mssg.message = req.body.message;  // set the bears name (comes from the request)
@@ -97,7 +268,28 @@ router.get('/messages',function(req, res) {
     });
 
 });
-// FIND AND UPDATE
+/**
+   * @swagger
+   * /messages/:id:
+   *   put:
+   *     description: updates a specific message
+   *     tags: [Message]
+   *     produces:
+   *       - application/json
+   *     parameters:
+   *       - $ref: '#/parameters/id'
+   *       - name: id
+   *         description: the documents id to Get
+   *         in: formData
+   *         required: true
+   *         type: string
+   *     responses:
+   *       200:
+   *         description: PUT
+   *         schema:
+   *           type: object
+   *           $ref: '#/definitions/message'
+   */
 router.put('/messages/:id', function(req,res) {
     Message.updateOne({_id: req.params.id}, req.body,function(err, doc) {
         if(err) {
@@ -106,7 +298,28 @@ router.put('/messages/:id', function(req,res) {
         res.send (doc)
     })
 })
-// FIND AND DELETE
+/**
+   * @swagger
+   * /messages/:id:
+   *   delete:
+   *     description: deletes a specific message
+   *     tags: [Message]
+   *     produces:
+   *       - application/json
+   *     parameters:
+   *       - $ref: '#/parameters/id'
+   *       - name: id
+   *         description: the documents id to Get
+   *         in: formData
+   *         required: true
+   *         type: string
+   *     responses:
+   *       200:
+   *         description: DELETE
+   *         schema:
+   *           type: object
+   *           $ref: '#/definitions/message'
+   */
 router.delete('/messages/:id', function(req, res) {
     Message.remove({
         _id: req.params.id
