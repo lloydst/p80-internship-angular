2766491d2df1c44cb51f5dc8590d1c83057ea60d
Lloyd Stumpel <lloyd.stumpel@poort80.nl>
2018-05-16 11:47:13 +0200

    updated to angular 6

--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import { Subject } from 'rxjs/Subject';
-import { Observable } from 'rxjs/Observable';
+import { Observable, Subject } from 'rxjs';
 import * as io from 'socket.io-client';
 
 /**
