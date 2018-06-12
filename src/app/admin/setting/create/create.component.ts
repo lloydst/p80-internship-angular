c53bbc4d107840caa5fbf374555d5d505ae73f26
lloydst <lloyd.stumpel@poort80.nl>
2018-06-12 16:42:26 +0200

    angular can create-delete ip configs

---
+++ src/app/admin/setting/create/create.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { IpService } from '../../../services/ip.service';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-create',
+  templateUrl: './create.component.html'
+})
+export class CreateComponent implements OnInit {
+    ipAddress 
+  constructor(private ipService: IpService,
+    private http: HttpClient) {
+    
+   }
+otherIps: any = []
+  ngOnInit() {
+    this.ipService.getIps().subscribe(configs=>{
+        this.otherIps = configs
+        console.log(configs)
+    })
+    this.http.get<{ ip: string }>('https://jsonip.com')
+    .subscribe(data => {
+
+        this.ipAddress = data.ip
+        
+    })
+  }
+  addIpConfig (obj) {
+        this.ipService.addIp(obj).subscribe()
+  }
+}
