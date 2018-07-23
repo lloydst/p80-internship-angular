d0539fd12a40fb262da8e40d4bcccb8805addf0e
lloydst <lloyd.stumpel@poort80.nl>
2018-07-23 10:34:59 +0200

    minor styling for admin components and fix to the meeting component

--- src/app/admin/setting/setting.component.ts
+++ src/app/admin/setting/setting.component.ts
@@ -15,47 +15,16 @@ export class SettingComponent implements OnInit {
      */
     constructor(private ipservice: IpService) { }
     configs
-
+iptest: string
     /**
      * on init
      */
     ngOnInit() {
+        
         this.getConfigs()
-        this.getUserIP(function (ip) {
-            document.getElementById("ip").innerHTML = 'Got your IP ! : ' + ip
-        });
+        
     }
-    getUserIP(onNewIP) { //  onNewIp - your listener function for new IPs
-        //compatibility for firefox and chrome
-        var myPeerConnection = (<any>window).RTCPeerConnection || (<any>window).mozRTCPeerConnection || (<any>window).webkitRTCPeerConnection;
-        var pc = new myPeerConnection({
-            iceServers: []
-        }),
-            noop = function () { },
-            localIPs = {},
-            ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
-            key;
-
-        function iterateIP(ip) {
-            if (!localIPs[ip]) onNewIP(ip);
-            localIPs[ip] = true;
-        }
-        //create a bogus data channel
-        pc.createDataChannel("");
-        // create offer and set local description
-        pc.createOffer(function (sdp) {
-            sdp.sdp.split('\n').forEach(function (line) {
-                if (line.indexOf('candidate') < 0) return;
-                line.match(ipRegex).forEach(iterateIP);
-            });
-            pc.setLocalDescription(sdp, noop, noop);
-        }, noop);
-        //listen for candidate events
-        pc.onicecandidate = function (ice) {
-            if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
-            ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
-        };
-    }
+    
     deleteConfig(ip) {
         this.ipservice.deleteIp(ip).subscribe()
     }
