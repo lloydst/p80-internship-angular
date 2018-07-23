02b17198f17dbe9fcd5d6bab0881c623f25aa7be
lloydst <lloyd.stumpel@poort80.nl>
2018-07-23 14:19:59 +0200

    minor admin style changes

--- src/app/components/website-loop/website-loop.component.ts
+++ src/app/components/website-loop/website-loop.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { DataService } from '../../services/data.service';
 import { Website } from '../../models/website';
