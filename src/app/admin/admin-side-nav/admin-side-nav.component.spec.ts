f3b67e34f5b50b693fd52b959d30c8af0b5387af
lloydst <lloyd.stumpel@poort80.nl>
2018-08-21 14:14:49 +0200

    different loader

--- src/app/admin/admin-side-nav/admin-side-nav.component.spec.ts
+++ src/app/admin/admin-side-nav/admin-side-nav.component.spec.ts
@@ -1,6 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AdminSideNavComponent } from './admin-side-nav.component';
-import { Router, RouterLink } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { By } from '@angular/platform-browser';
 
@@ -23,9 +22,6 @@ describe('AdminSideNavComponent', () => {
         fixture.detectChanges();
         // get a list of the router links and check if it navigates to the right endpoint
         navLinks = fixture.debugElement.queryAll(By.css('a'));
-        console.log(navLinks);
-        // expect(dirLinks[0].linkParams[0]).toBe('/admin', 'Link to Admin Home Page');
-        // expect(dirLinks[1].linkParams[0]).toBe('/my-questions', 'Link to My Questions');
     });
 
     it('should create', () => {
@@ -39,11 +35,7 @@ describe('AdminSideNavComponent', () => {
         expect(navLinks[2].properties.href).toBe('/components/entrance');
         expect(navLinks[3].properties.href).toBe('/channels');
         expect(navLinks[4].properties.href).toBe('/upload');
-        // expect(navLinks[5].properties.href).toEqual('')
-        // expect(navLinks[6].properties.href).toBe('/javascript:void(0);')
-
         // dont forget to navigate back in between
-
     });
     it('should expand when the last is clicked', () => {
         // check if classes change
