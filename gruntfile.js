460b40d272c43c50999b776b1d6cd888daf28987
lloydst <lloyd.stumpel@poort80.nl>
2018-10-16 15:42:28 +0200

    does heroku use compiled now instead

--- gruntfile.js
+++ gruntfile.js
@@ -20,6 +20,7 @@ module.exports = function(grunt) {
             },
         },
          sass: {
+            
             dev: {
                 options: {
                     outputStyle: 'expanded',
