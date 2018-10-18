c07b39e442faf2f2b1bdd950ecd09fe83a927377
lloydst <lloyd.stumpel@poort80.nl>
2018-10-18 15:33:59 +0200

    derp

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-      browsers: ['ChromeHeadless'], //ChromeHeadless
+      browsers: ['Chrome'], //ChromeHeadless
     singleRun: true // true
   });
 };
